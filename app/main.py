89cedc55c7f2e2cccbb7299a9dd7e7cf5b750185
Alex <18anguyen9@gmail.com>
2022-06-22 21:59:34 +0000

    updated website

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from flask import send_from_directory
-from flask import Flask, flash, request, redirect, url_for
-from werkzeug.utils import secure_filename
+from flask import Flask, flash, request, redirect, url_for, session
 from flask import render_template
 from url_utils import get_base_url
-import os
+from werkzeug.utils import secure_filename
+import os, shutil
 import torch
 
 # setup the webserver
@@ -11,103 +11,141 @@ import torch
 port = 12345
 base_url = get_base_url(port)
 
-# if the base url is not empty, then the server is running in development, and we need to specify the static folder so that the static files are served
+
+# if the base url is not empty, then the server is running in development, so specify the static folder to serve static files.
 if base_url == '/':
     app = Flask(__name__)
 else:
     app = Flask(__name__, static_url_path=base_url+'static')
 
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.secret_key = 'abc'
+
 UPLOAD_FOLDER = 'static/uploads'
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'tif', 'tiff'])
+OUTPUT_IMAGE = "output.jpg"
+
+# clear upload folder
+for filename in os.listdir(UPLOAD_FOLDER):
+    file_path = os.path.join(UPLOAD_FOLDER, filename)
+    try:
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+    except Exception:
+        continue
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 32 * 1024 * 1024
+app.config['MAX_CONTENT_LENGTH'] = 32 * 1024 * 1024 # 2**25
 
-model = torch.hub.load("ultralytics/yolov5", "custom", path = 'best.pt', force_reload=True)
+model = torch.hub.load('ultralytics/yolov5', 'custom', path = 'best.pt', force_reload=True)
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+minimum_confidence = 0.25
+
+def check_ext(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route(f'{base_url}', methods=['GET', 'POST'])
+def sendToHome():
+    return redirect(url_for('home'))
+@app.route(f'{base_url}/home', methods=['GET', 'POST'])
 def home():
+    return render_template('mainhome.html')
+@app.route(f'{base_url}/timeline', methods=['GET', 'POST'])
+def timeline():
+    return render_template('timeline.html')
+@app.route(f'{base_url}/teampage', methods=['GET', 'POST'])
+def teampage():
+    return render_template('teampage.html')
+
+@app.route(f'{base_url}/detect', methods=['GET', 'POST'])
+def detect():
+    global minimum_confidence
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
-
         file = request.files['file']
         # if user does not select file, browser also
         # submit an empty part without filename
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
-
-        if file and allowed_file(file.filename):
+        # check extension
+        if file and check_ext(file.filename):
+            minimum_confidence = int(request.form['confidence']) / 100
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return render_template('detect.html')
 
-    return render_template('home.html')
-
-
+# when a file appears in static/uploads/
 @app.route(f'{base_url}/uploads/<filename>')
 def uploaded_file(filename):
     here = os.getcwd()
     image_path = os.path.join(here, app.config['UPLOAD_FOLDER'], filename)
+
+    model.conf = minimum_confidence
+
     results = model(image_path, size=416)
     if len(results.pandas().xyxy) > 0:
         results.print()
         save_dir = os.path.join(here, app.config['UPLOAD_FOLDER'])
+        # os.remove(image_path)
         results.save(save_dir=save_dir)
-        def and_syntax(alist):
-            if len(alist) == 1:
-                alist = "".join(alist)
-                return alist
-            elif len(alist) == 2:
-                alist = " and ".join(alist)
-                return alist
-            elif len(alist) > 2:
-                alist[-1] = "and " + alist[-1]
-                alist = ", ".join(alist)
-                return alist
-            else:
-                return
         confidences = list(results.pandas().xyxy[0]['confidence'])
-        # confidences: rounding and changing to percent, putting in function
+
         format_confidences = []
-        for percent in confidences:
-            format_confidences.append(str(round(percent*100)) + '%')
+        # convert to nearest percent as string
+        for decimal in confidences:
+            format_confidences.append(str(round(decimal * 100)) + '%')
+        # format the labels to sort, capitalize
+        labels = set(list(results.pandas().xyxy[0]['name']))
+        labels = [car.capitalize() for car in labels]
+        # join items with 'and' between them
+        def and_syntax(str_list):
+            if len(str_list) == 1:
+                return "".join(str_list)
+            if len(str_list) == 2:
+                return " and ".join(str_list)
+            if len(str_list) > 2:
+                str_list[-1] = "and " + str_list[-1]
+                return ", ".join(str_list)
+            return ''
         format_confidences = and_syntax(format_confidences)
-
-        labels = list(results.pandas().xyxy[0]['name'])
-        # labels: sorting and capitalizing, putting into function
-        labels = set(labels)
-        labels = [emotion.capitalize() for emotion in labels]
         labels = and_syntax(labels)
-        return render_template('results.html', confidences=format_confidences, labels=labels,
-                               old_filename=filename,
-                               filename=filename)
-    else:
-        found = False
-        return render_template('results.html', labels='No Emotion', old_filename=filename, filename=filename)
-
+        # model output filename should always have .jpg
+        new_filename, extension = os.path.splitext(image_path)
+        new_filename += ".jpg"
+        output_path = os.path.join(here, app.config['UPLOAD_FOLDER'], OUTPUT_IMAGE)
+        os.rename(new_filename, output_path)
+        if len(confidences) > 0:
+            return render_template('results.html',
+                                   confidences=format_confidences,
+                                   labels=labels,
+                                   filename=OUTPUT_IMAGE,
+                                   min_conf=(minimum_confidence * 100))
+        else:
+            return render_template('results.html',
+                                   labels='no cars',
+                                   confidences='100%',
+                                   filename=OUTPUT_IMAGE,
+                                   min_conf=minimum_confidence*100)
+    found = False
+    return render_template('results.html',
+                           labels='no cars',
+                           confidences='high',
+                           filename=filename,
+                           min_conf=minimum_confidence * 100)
 
 @app.route(f'{base_url}/uploads/<path:filename>')
 def files(filename):
     return send_from_directory(UPLOAD_FOLDER, filename, as_attachment=True)
 
-# define additional routes here
-# for example:
-# @app.route(f'{base_url}/team_members')
-# def team_members():
-#     return render_template('team_members.html') # would need to actually make this page
-
 if __name__ == '__main__':
     # IMPORTANT: change url to the site where you are editing this file.
-    website_url = 'url'
-    
+    website_url = 'cocalc10.ai-camp.dev'
     print(f'Try to open\n\n    https://{website_url}' + base_url + '\n\n')
     app.run(host = '0.0.0.0', port=port, debug=True)
---
+++ app/static/js/scripts.js
@@ -0,0 +1,270 @@
+/*!
+* Start Bootstrap - Grayscale v7.0.5 (https://startbootstrap.com/theme/grayscale)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
+*/
+
+// -------- navbar --------
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    function navbarShrink(){
+        const navbarCollapsible = document.body.querySelector('nav');
+        if (!navbarCollapsible){ return; }
+        if(window.scrollY === 0){
+            navbarCollapsible.classList.remove('navbar-shrink');
+        }else{
+            navbarCollapsible.classList.add('navbar-shrink');
+        }
+    }
+
+    // Shrink the navbar
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the nav element
+    const nav = document.body.querySelector('nav');
+    if(nav){
+        new bootstrap.ScrollSpy(document.body, {
+            target: 'nav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+// -------- Drag and Drop scripts -------- //
+
+let dropArea = document.getElementById("drop-area");
+// Prevent default drag behaviors
+['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+  dropArea.addEventListener(eventName, preventDefaults, false)
+  document.body.addEventListener(eventName, preventDefaults, false)
+});
+
+// Highlight drop area when item is dragged over it
+['dragenter', 'dragover'].forEach(eventName => {
+  dropArea.addEventListener(eventName, highlight, false)
+});
+
+['dragleave', 'drop'].forEach(eventName => {
+  dropArea.addEventListener(eventName, unhighlight, false)
+})
+
+// Handle dropped files
+dropArea.addEventListener('drop', handleDrop, false)
+
+function preventDefaults (e) {
+  e.preventDefault()
+  e.stopPropagation()
+}
+
+function highlight(e) {
+  dropArea.classList.add('highlight')
+}
+
+function unhighlight(e) {
+  dropArea.classList.remove('active')
+}
+
+function handleDrop(e) {
+  var dt = e.dataTransfer
+  var files = dt.files
+
+  handleFiles(files)
+}
+
+let uploadProgress = []
+let progressBar = document.getElementById('progress-bar')
+
+function initializeProgress(numFiles) {
+  progressBar.value = 0
+  uploadProgress = []
+
+  for(let i = numFiles; i > 0; i--) {
+    uploadProgress.push(0)
+  }
+}
+
+function updateProgress(fileNumber, percent) {
+  uploadProgress[fileNumber] = percent
+  let total = uploadProgress.reduce((tot, curr) => tot + curr, 0) / uploadProgress.length
+  progressBar.value = total
+}
+
+function handleFiles(files) {
+  files = [...files]
+  initializeProgress(files.length)
+  files.forEach(uploadFile)
+  files.forEach(previewFile)
+}
+
+function previewFile(file) {
+  let reader = new FileReader()
+  reader.readAsDataURL(file)
+  reader.onloadend = function() {
+    let img = document.createElement('img')
+    img.src = reader.result
+    document.getElementById('gallery').appendChild(img)
+  }
+}
+
+function uploadFile(file, i) {
+  var url = 'https://cocalc10.ai-camp.dev/projects/bf0b3665-37be-49f4-895a-fd6a70d9dc4a/files/startbootstrap-grayscale-gh-pages/'
+  var xhr = new XMLHttpRequest()
+  var formData = new FormData()
+  xhr.open('POST', url, true)
+  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
+
+  // Update progress (can be used to show progress indicator)
+  xhr.upload.addEventListener("progress", function(e) {
+    updateProgress(i, (e.loaded * 100.0 / e.total) || 100)
+  })
+
+  xhr.addEventListener('readystatechange', function(e) {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      updateProgress(i, 100) // <- Add this
+    }
+    else if (xhr.readyState == 4 && xhr.status != 200) {
+      // Error. Inform the user
+    }
+  })
+
+  formData.append('upload_preset', 'ujpu6gyk')
+  formData.append('file', file)
+  xhr.send(formData)
+}
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+});
+
+document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
+    const dropZoneElement = inputElement.closest(".drop-zone");
+  
+    dropZoneElement.addEventListener("click", (e) => {
+      inputElement.click();
+    });
+  
+    inputElement.addEventListener("change", (e) => {
+      if (inputElement.files.length) {
+        updateThumbnail(dropZoneElement, inputElement.files[0]);
+      }
+    });
+  
+    dropZoneElement.addEventListener("dragover", (e) => {
+      e.preventDefault();
+      dropZoneElement.classList.add("drop-zone--over");
+    });
+  
+    ["dragleave", "dragend"].forEach((type) => {
+      dropZoneElement.addEventListener(type, (e) => {
+        dropZoneElement.classList.remove("drop-zone--over");
+      });
+    });
+  
+    dropZoneElement.addEventListener("drop", (e) => {
+      e.preventDefault();
+  
+      if (e.dataTransfer.files.length) {
+        inputElement.files = e.dataTransfer.files;
+        updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
+      }
+  
+      dropZoneElement.classList.remove("drop-zone--over");
+    });
+  });
+  
+  /**
+   * Updates the thumbnail on a drop zone element.
+   *
+   * @param {HTMLElement} dropZoneElement
+   * @param {File} file
+   */
+  function updateThumbnail(dropZoneElement, file) {
+    let thumbnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
+  
+    // First time - remove the prompt
+    if (dropZoneElement.querySelector(".drop-zone__prompt")) {
+      dropZoneElement.querySelector(".drop-zone__prompt").remove();
+    }
+  
+    // First time - there is no thumbnail element, so lets create it
+    if (!thumbnailElement) {
+      thumbnailElement = document.createElement("div");
+      thumbnailElement.classList.add("drop-zone__thumb");
+      dropZoneElement.appendChild(thumbnailElement);
+    }
+  
+    thumbnailElement.dataset.label = file.name;
+  
+    // Show thumbnail for image files
+    if (file.type.startsWith("image/")) {
+      const reader = new FileReader();
+  
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
+      };
+    } else {
+      thumbnailElement.style.backgroundImage = null;
+    }
+  }
+  
